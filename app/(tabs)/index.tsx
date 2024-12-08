f6c3fd346ff296c634ac9c0ce2aa5b9e0415d884
Antonio Fulgencio <antoniofulg@gmail.com>
2024-12-07 22:59:24 -0300

    Add components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,20 @@
-import { H1 } from "@/components/Typography"
-import { Link, Stack } from "expo-router"
+import { View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
+import ImageViewer from "./components/ImageViewer"
+import Button from "./components/Button"
+
+const PlaceholderImage = require("@/assets/images/background-image.png")
 
 export default function HomeScreen() {
 	return (
-		<>
-			<Stack.Screen options={{ title: "Home" }} />
-			<SafeAreaView className="flex-1 justify-center items-center dark:bg-black">
-				<H1>First app</H1>
-				<Link href="/about">Go to About</Link>
-			</SafeAreaView>
-		</>
+		<SafeAreaView className="flex-1 justify-center items-center dark:bg-black">
+			<View className="flex-1">
+				<ImageViewer imgSource={PlaceholderImage} />
+			</View>
+			<View className="flex-[1/3] items-center">
+				<Button label="Choose a photo" />
+				<Button label="Use this photo" />
+			</View>
+		</SafeAreaView>
 	)
 }
